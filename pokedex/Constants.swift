7f99342206f738bed1163c2b64624f4ce83c20c7
Timm Liberty <timmliberty@timms-imac.local>
2016-11-16 14:19:11 -0500

    Finished. Only moves left to do.

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Timm Liberty on 11/16/16.
+//  Copyright © 2016 Briantiumapps. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -28,13 +28,44 @@ class PokemonDetailVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        nameLabel.text = pokemon.name
+        nameLabel.text = pokemon.name.capitalized
         
+        let img = UIImage(named: "\(pokemon.pokedexID)")
+        
+        mainImage.image = img
+        currentEvoImg.image = img
+        pokedexLabel.text = "\(pokemon.pokedexID)"
+        
+        pokemon.downloadPokemonDetail {
+            
+            // Whatever we write will only be called after the network call is complete.
+            self.updateUI()
+        }
     }
 
     @IBAction func backButtonPressed(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
 
-
+    func updateUI(){
+        attackLabel.text = pokemon.attack
+        defenseLabel.text = pokemon.defense
+        heightLabel.text = pokemon.height
+        weightLabel.text = pokemon.weight
+        typeLabel.text = pokemon.type
+        descriptionLabel.text = pokemon.description
+        
+        if pokemon.nextEvolutionId == "" {
+            evoLabel.text = "No Evolutions"
+            nextEvoImg.isHidden = true
+        } else {
+            nextEvoImg.isHidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            let str = "Next Evolution: \(pokemon.nextEvolutionName) - LVL \(pokemon.nextEvolutionLevel)"
+            evoLabel.text = str
+        }
+        
+        
+        
+    }
 }
